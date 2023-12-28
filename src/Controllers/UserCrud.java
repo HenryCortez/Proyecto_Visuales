8600ffb54fb929f05eebabda4e190c4d6262fd02
Hamilton Jumbo <hamil.jumbo@gmail.com>
2023-12-28 12:08:38 -0500

    Implementacion del metodo de updateUser

--- src/Controllers/UserCrud.java
+++ src/Controllers/UserCrud.java
@@ -18,9 +18,11 @@ import javax.swing.JOptionPane;
  */
 public class UserCrud {
     Conexion con = new Conexion();
-    
-        public void insertUser (String cedula, String nombre, String apellido, String contrasenia){
-            try {
+    //1.- Primer metodo que me permita insertar un usuario
+        public void insertUser (String cedula, String nombre, String apellido, String contrasenia)
+        {
+            try 
+            {
                 con.conectar();
                 String sql= "INSERT INTO USUARIOS (CED_USU,NOM_USU, APE_USU, CON_USU) VALUES (?,?,?,?);";
                 PreparedStatement ps = con.getCon().prepareStatement(sql);
@@ -28,6 +30,7 @@ public class UserCrud {
                 ps.setString(2, nombre);
                 ps.setString(3, apellido);
                 ps.setString(4, contrasenia);
+                //Controlamos el numero de respuesta para poder mandar mensjaes de errores
                 int insercion = ps.executeUpdate();
                 if(insercion > 0)
                 {
@@ -37,12 +40,43 @@ public class UserCrud {
                 {
                     JOptionPane.showMessageDialog(null, "Error al crear. Verifica los datos e intenta nuevamente.");
                 }
-            } catch (SQLException ex) {
-                Logger.getLogger(UserCrud.class.getName()).log(Level.SEVERE, null, ex);
-                JOptionPane.showMessageDialog(null, "Error al guardar. Consulta los registros del sistema para obtener más detalles.");
-            } finally
+            } catch (SQLException ex) 
                 {
-                    con.desconectar();
-                }
+                    Logger.getLogger(UserCrud.class.getName()).log(Level.SEVERE, null, ex);
+                    JOptionPane.showMessageDialog(null, "Error al guardar. Consulta los registros del sistema para obtener más detalles.");
+                } finally
+                    {
+                        con.desconectar();
+                    }
         }
+        
+    //2.- Segundo metodo que me permita atualizar un usuario   
+        public void UpdateUser (String cedula, String nombre, String apellido, String contrasenia)
+        {
+            try 
+            {
+                con.conectar();
+                String sql = "UPDATE USUARIOS SET NOM_USU=?, APE_USU=?, CON_USU=? WHERE ID_USU=?";
+                PreparedStatement ps = con.getCon().prepareStatement(sql);
+                ps.setString(1, nombre);
+                ps.setString(2, apellido);
+                ps.setString(3, contrasenia);
+                ps.setString(4, cedula);
+                //Controlamos el numero de respuesta para poder mandar mensjaes de errores
+                int actualizacion = ps.executeUpdate();
+                if (actualizacion > 0) {
+                    JOptionPane.showMessageDialog(null, "Usuario actualizado correctamente");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Error al actualizar. Verifica los datos e intenta nuevamente.");
+                }
+            } catch (SQLException ex) 
+            {
+                Logger.getLogger(UserCrud.class.getName()).log(Level.SEVERE, null, ex);
+                JOptionPane.showMessageDialog(null, "Error al actualizar. Consulta los registros del sistema para obtener más detalles.");
+            }finally
+            {
+                con.desconectar();
+            }
+        }
+    
 }
